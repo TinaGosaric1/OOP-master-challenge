318bd1f1c6600a06afcde3591246ddc7ad0c6eaf
Tina Gosarič <tina.gosaric@gorenje.com>
2022-10-17 16:47:24 +0200

    Adding 3 classes.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import razredi.DeluxeBurger;
+import razredi.Hamburger;
+import razredi.HealthyBurger;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Hamburger hambi = new Hamburger("Basic", "Beef", 3.56, "White");
+        double hamPrice = hambi.itemizeHamburger();
+        hambi.addHamburgerAddition1("Tomato", 0.27);
+        hambi.addHamburgerAddition2("Lettuce", 0.75);
+        hambi.addHamburgerAddition3("Cheese", 1.13);
+        System.out.println("Total Burger price is " + hambi.itemizeHamburger() + "\n");
+
+        HealthyBurger hambi2 = new HealthyBurger("Chicken", 5.67);
+        hambi2.addHamburgerAddition1("Egg", 5.43);
+        hambi2.addHealthAddition1("Lentils", 3.41);
+        System.out.println("Total Burger price is " + hambi2.itemizeHamburger() + "\n");
+
+        DeluxeBurger deluxeBurger = new DeluxeBurger();
+        deluxeBurger.addHamburgerAddition3("Add something.", 50.53);
+        deluxeBurger.itemizeHamburger();
     }
 }
